8e17c01176d7a9550677470dd5165115f6b8a0d6
Turtle Code Youtube <85156399+turtlecode@users.noreply.github.com>
2025-09-09 13:53:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from supabase import create_client, Client
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
